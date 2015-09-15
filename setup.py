2935947cbbfb4deb66d82f12564a1d996874c18f
Titov Evgeniy <titov.evgeniy@bdbd.ru>
2015-09-15 13:48:11 +0300

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = "0.0.1"
 
 setup(
     name='sso-client-decorator',
-    description='сlient decorator for sso authentication scheme',
+    description=u'client decorator for sso authentication scheme',
     version=VERSION,
     url='https://github.com/KokocGroup/sso-client-decorator',
     download_url='https://github.com/KokocGroup/sso-client-decorator/tarball/v{0}'.format(VERSION),
